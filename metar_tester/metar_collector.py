923bc9b18bfd84b32b908dd4004cdbfc8ed88a9c
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2021-09-03 18:26:52 +0100

    Question generation almost completed still need to impletement active wx codes

--- metar_tester/metar_collector.py
+++ metar_tester/metar_collector.py
@@ -1,9 +1,171 @@
+import django
 import json
 import requests
 import os
+import sys
+import random
+
+sys.path.append('..')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rhodrithomasmorgan.settings'
+django.setup()
+
+from metar_tester.models import Airport
 
 
-x = requests.get('https://avwx.rest/api/metar/EGLL?options=&airport=true&reporting=true&format=json&onfail=cache',
-                 headers={'Authorization':os.environ.get('METAR_KEY')})
-print(x.text)
+class METAR_colllector:
 
+    # Gets METAR data for given ICAO in a dict format
+    def get_data(self, icao):
+        url = 'https://avwx.rest/api/metar/{0}?options=&airport=true&reporting=true&format=json&onfail=cache'.format(icao)
+        data = requests.get(url, headers={'Authorization':os.environ.get('METAR_KEY')})
+        if data.status_code == 200:
+            return json.loads(data.text)
+        elif data.status_code == 503:
+            return '503'
+        else:
+            Airport.objects.filter(icao=icao).delete()
+            print('Removed airport {0}'.format(icao))
+            return None
+
+
+    # Gets a random airport from the database
+    def get_random_airport(self):
+        return Airport.objects.order_by('?').first()
+
+
+    # Generates a question for the user to attempt
+    def generate_questions(self, icao, data):
+        questions = {}
+
+        if 'units' not in data:
+            return None
+
+        if 'station' in data:
+            questions['airport'] = {'question' : 'What is the airport ICAO?',
+                                    'answer' : [data['station']],
+                                    'answer_units' : '',
+                                    'choices': []}
+        if 'time' in data:
+            questions['time'] = {'question' : 'What is time was this METAR report made?',
+                                 'answer' : [data['time']['repr'][2:-1]],
+                                 'answer_units' : 'ZULU',
+                                 'choices': []}
+        if 'wind_direction' in data:
+            questions['wind_direction'] = {'question' : 'What is the wind direction?',
+                                           'answer' : [str(data['wind_direction']['value'])],
+                                           'answer_units' : 'degrees',
+                                           'choices': []}
+        if 'wind_gust' in data:
+            gust = None
+            if data['wind_gust'] is not None:
+                gust = str(data['wind_gust']['value'])
+            else:
+                gust = 0
+
+            questions['wind_gust'] = {'question' : 'What is the wind gusting to?',
+                                      'answer' : [gust],
+                                      'answer_units' : data['units']['wind_speed'],
+                                      'choices': []}
+        if 'wind_speed' in data:
+            questions['wind_speed'] = {'question' : 'What is the wind speed?',
+                                       'answer' : [str(data['wind_speed']['value'])],
+                                       'answer_units' : data['units']['wind_speed'],
+                                       'choices': []}
+        if 'altimiter' in data:
+            questions['altimeter'] = {'question' : 'What is the altimiter?',
+                                      'answer' : [str(data['altimeter']['value'])],
+                                      'answer_units' : data['units']['altimeter'],
+                                      'choices': []}
+        if 'temperature' in data:
+            questions['temperature'] = {'question' : 'What is the temperature?',
+                                      'answer' : [str(data['temperature']['value'])],
+                                      'answer_units' : data['units']['temperature'],
+                                      'choices': []}
+        if 'dewpoint' in data:
+            questions['dewpoint'] = {'question' : 'What is the dewpoint?',
+                                     'answer' : [str(data['dewpoint']['value'])],
+                                     'answer_units' : data['units']['temperature'],
+                                     'choices': []}
+        if 'visability' in data:
+            questions['dewpoint'] = {'question' : 'What is the visiability?',
+                                     'answer' : [str(data['visibility']['value'])],
+                                     'answer_units' : data['units']['visibility'],
+                                     'choices': []}
+        if 'clouds' in data:
+            cloud_heights = {'few': [], 'scattered': [], 'broken': [], 'overcast': []}
+
+            for item in data['clouds']:
+                cloud_type = None
+                if item['type'] == 'FEW':
+                    cloud_heights['few'].append(str(item['altitude'])+'00')
+                elif item['type'] == 'SCT':
+                    cloud_heights['scattered'].append(str(item['altitude'])+'00')
+                elif item['type'] == 'BKN':
+                    cloud_heights['broken'].append(str(item['altitude'])+'00')
+                elif item['type'] == 'OVC':
+                    cloud_heights['overcast'].append(str(item['altitude'])+'00')
+
+            questions['cloud_few'] = {'question' : 'What is the height of the few clouds?',
+                                        'answer' : [cloud_heights['few']],
+                                        'answer_units' : data['units']['altitude'],
+                                        'choices': []}
+
+            questions['cloud_scattered'] = {'question' : 'What is the height of the scattered clouds?',
+                                            'answer' : [cloud_heights['scattered']],
+                                            'answer_units' : data['units']['altitude'],
+                                            'choices': []}
+
+            questions['cloud_broken'] = {'question' : 'What is the height of the broken clouds?',
+                                            'answer' : [cloud_heights['broken']],
+                                            'answer_units' : data['units']['altitude'],
+                                            'choices': []}
+
+            questions['cloud_overcast'] = {'question' : 'What is the height of the overcast clouds?',
+                                            'answer' : [cloud_heights['overcast']],
+                                            'answer_units' : data['units']['altitude'],
+                                            'choices': []}
+
+            count = 0
+            for key, value in cloud_heights.items():
+                for height in value:
+                    questions['cloud_{0}'.format(count)] = {'question' : 'What kind of clouds have a ceiling of {0} ft?'.format(height),
+                                                            'answer' : [key.upper()],
+                                                            'answer_units' : data['units']['altitude'],
+                                                            'choices': [key.capitalize() for key in cloud_heights.keys()]}
+                    count += 1
+
+        if len(questions) >= 1:
+            return questions
+        else:
+            return None
+
+
+metar_colllector = METAR_colllector()
+icao = None
+data = None
+questions = None
+
+while True:
+    icao = metar_colllector.get_random_airport().icao
+
+    if icao is not None:
+        data = metar_colllector.get_data(icao)
+
+    if data == '503':
+        print('Flag website the API METAR service is down.')
+    elif data is not None:
+        questions = metar_colllector.generate_questions(icao, data)
+
+    if questions is not None:
+        print('{0}\n{1}'.format(json.dumps(data, indent=4), json.dumps(questions, indent=4)))
+        exit()
+
+
+'''
+NOTES TO SELF
+
+- TRICK QUESTIONS ARE INCLUDED LIKE FOR CLOUDS IF THERE ARE NO FEW CLOUDS THEN 'ANSWER' FIELD WILL BE EMPTY. THESE WILL NEED TO BE
+ORDERED BEFORE ANY QUESTIONS RELATING TO HEIGHT OF CLOUDS OR WHAT KIND OF CLOUDS HAVE CEILING OF X FT. THIS CAN BE ACHIEVED/CHECKED
+IN VIEWS
+- NEED TO INCLUDE WX LIKE RAIN
+'''
