061b875307d53a671a387f187764bb0fccd8391f
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2021-09-04 13:27:13 +0100

    Added js to fix navigation bar width

--- static/js/metar_tester/sub_base.js
+++ static/js/metar_tester/sub_base.js
@@ -1,3 +1,15 @@
-function start_new() {
-    alert("Begin new test");
+window.onload = updateNavigationWidth;
+
+
+/**
+ * Bodging to fixup the navigation bar width
+ */
+function updateNavigationWidth() {
+    var navigation_bar = document.getElementById('navigation');
+    var elements = document.getElementsByClassName("navigation_element");
+    var total_elements_width = 0
+    for (var i = 0; i < elements.length; i++) {
+        total_elements_width += elements[i].offsetWidth;
+    }
+    navigation_bar.style.width = total_elements_width.toString(10)+"px";
 }
