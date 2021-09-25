c7ef9e1da5a4b375664b5ae9cc7fae92ee8da6c5
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2021-09-25 16:17:20 +0100

    Added script for pulling and storing data at hourly intervals

---
+++ metar_practice/pull_metar_data.py
@@ -0,0 +1,52 @@
+import django
+import json
+import requests
+import os
+import datetime
+import sys
+
+sys.path.append('..')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rhodrithomasmorgan.settings'
+django.setup()
+
+from metar_practice.metar_collector import MetarCollector
+from metar_practice.question_collector import QuestionColllector
+
+
+class PullMetarData:
+
+    def __init__(self):
+        self.metar_collector = MetarCollector()
+        self.hour_pull_count = 10
+
+
+    def main(self):
+        next_pull = datetime.datetime.utcnow()
+        while True:
+            time_now = datetime.datetime.utcnow()
+            if time_now >= next_pull:
+                print('Beginning pulling {0} METAR data'.format(time_now))
+                for i in range (0, self.hour_pull_count):
+                    while True:
+                        db_airport = self.metar_collector.get_random_airport()
+                        db_metar = None
+                        db_questions = None
+
+                        if db_airport is not None:
+                            status, db_metar = self.metar_collector.get_raw_metar(db_airport)
+
+                        if db_metar is not None:
+                            metar = json.loads(db_metar.metar_json)
+                            question_colllector = QuestionColllector(db_metar, None)
+                            db_questions = question_colllector.generate_questions()
+
+                        if db_questions is not None and len(db_questions) != 0:
+                            break
+                    print('Successfully pulled {0}/{1}'.format(i+1, self.hour_pull_count))
+                print('Successfully completed {0} METAR data pull'.format(time_now))
+                next_pull = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+
+
+if __name__ == '__main__':
+    pull_metar_data = PullMetarData()
+    pull_metar_data.main()
