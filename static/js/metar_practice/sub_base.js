ca58dbfbea89bc34bf7659cafe4b13d314e0d700
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2021-10-08 20:31:26 +0100

    Fixed bug with issue message and added updated usage video

--- static/js/metar_practice/sub_base.js
+++ static/js/metar_practice/sub_base.js
@@ -1,17 +1,24 @@
-window.onload = updateNavigationWidth;
-window.onresize = updateNavigationWidth;
-
-
 /**
  * Bodging to fixup the navigation bar width
  */
-function updateNavigationWidth() {
+ window.setInterval(function updateNavigationWidth() {
     var navigation_bar = document.getElementById('navigation');
     var elements = document.getElementsByClassName("navigation_element");
     var total_elements_width = 0
     for (var i = 0; i < elements.length; i++) {
         total_elements_width += elements[i].offsetWidth;
     }
-    total_elements_width += 1;
+    total_elements_width += 2;
     navigation_bar.style.width = total_elements_width.toString()+"px";
-}
+}, 1);
+
+window.setInterval(function updateLoggedWidth() {
+    try {
+        var content_containter = document.getElementById('content').offsetWidth;
+        var page_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        var logged_container = document.getElementById('logged_container');
+        logged_container.style.width = Math.max(content_containter, page_width)+'px';
+    } catch (exception) {
+        //pass
+    }
+}, 1);
