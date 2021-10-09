99c42dbdd82278444453b95468a91f7a5dea84da
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2021-10-09 01:13:07 +0100

    Fixed footer for all browsers and updated usage video

--- static/js/metar_practice/sub_base.js
+++ static/js/metar_practice/sub_base.js
@@ -14,10 +14,15 @@
 
 window.setInterval(function updateLoggedWidth() {
     try {
-        var content_containter = document.getElementById('content').offsetWidth;
+        var content_containter_width = document.getElementById('content').offsetWidth;
         var page_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
         var logged_container = document.getElementById('logged_container');
-        logged_container.style.width = Math.max(content_containter, page_width)+'px';
+        logged_container.style.width = Math.max(content_containter_width, page_width)+'px';
+        if (content_containter_width < page_width) {
+            logged_container.style.left = ((content_containter_width-page_width)/2)+'px';
+        } else {
+            logged_container.style.left = 0+'px';
+        }
     } catch (exception) {
         //pass
     }
