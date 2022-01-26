67120664c31d468bf685cbeec1fdc48c148b9449
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2022-01-26 15:41:13 +0000

    Added question cap and reduced DB

--- metar_practice/pull_metar_data.py
+++ metar_practice/pull_metar_data.py
@@ -8,6 +8,9 @@ sys.path.append('..')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'rhodrithomasmorgan.settings'
 django.setup()
 
+from metar_practice.models import Metar
+from metar_practice.models import Question
+
 from metar_practice.metar_collector import MetarCollector
 from metar_practice.question_collector import QuestionCollector
 
@@ -32,4 +35,21 @@ for i in range (0, hour_pull_count):
         if status == 429 or (db_questions is not None and len(db_questions) != 0):
             break
     print('\nSuccessfully pulled {0}/{1}\n'.format(i+1, hour_pull_count))
+
+print('\nRemoving overflow of METAR data')
+database_question_limit = 100000
+db_answers = []
+while Question.objects.count() > database_question_limit:
+    db_metar = Metar.objects.order_by("?").first()
+    db_questions = Question.objects.filter(metar=db_metar)
+    for db_question in db_questions:
+        for db_answer in db_question.answers.all():
+            if db_answer not in db_answers:
+                db_answers.append(db_answer)
+    db_metar.delete()
+
+for db_answer in db_answers:
+    db_questions = Question.objects.filter(answers=db_answer)
+    if len(db_questions) == 0:
+        db_answer.delete()
 print('\nSuccessfully completed {0} METAR data pull\n'.format(time_now))
