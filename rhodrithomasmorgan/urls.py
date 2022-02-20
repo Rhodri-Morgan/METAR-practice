6ee348e95d7ccda7bb50bac691a4feb00ef24009
Rhodri-Morgan <rhodrithomasmorgan@protonmail.com>
2022-02-20 01:18:09 +0000

    Removed main references (from complete site)

--- rhodrithomasmorgan/urls.py
+++ rhodrithomasmorgan/urls.py
@@ -10,6 +10,3 @@ urlpatterns = [
     path('', include(metar_practice_urls)),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
-
-handler404 = 'main.views.error404'
-handler500 = 'main.views.error500'
